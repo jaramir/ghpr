7a6e5c1484962ecea74c5d4f1d321e764f472600
Francesco Gigli <jaramir@gmail.com>
2017-03-23 15:53:55 +0000

    list pull requests

--- public/app.js
+++ public/app.js
@@ -9,40 +9,7 @@ $.ajaxSetup({
 });
 var base_url = 'https://api.github.com';
 
-$('.orgs').on('click', function (e) {
-    var el = $(e.target)
-    var id = el.data("id")
-
-    $.ajax(base_url + '/orgs/' + id + '/teams')
-      .done(function (data) {
-        $('.teams').empty()
-
-        data.forEach(function (team) {
-          var el = $("<div></div>")
-          el.text(team.name)
-          el.data("id", team.id)
-          $('.teams').append(el)
-        })
-      })
-});
-
-$('.teams').on('click', function (e) {
-    var el = $(e.target)
-    var id = el.data("id")
-
-    $.ajax(base_url + '/teams/' + id + '/repos')
-      .done(function (data) {
-        $('.repos').empty()
-
-        data.forEach(function (repo) {
-          var el = $("<div></div>")
-          el.text(repo.name)
-          $('.repos').append(el)
-        })
-      })
-});
-
-$(function() {
+function fetch_orgs () {
   $.ajax(base_url + '/user/orgs')
     .done(function (data) {
       data.forEach(function (org) {
@@ -52,4 +19,58 @@ $(function() {
         $('.orgs').append(el)
       })
     })
+}
+
+function fetch_teams (org) {
+  $.ajax(base_url + '/orgs/' + org + '/teams')
+    .done(function (data) {
+      $('.teams').empty()
+
+      data.forEach(function (team) {
+        var el = $("<div></div>")
+        el.text(team.name)
+        el.data("id", team.id)
+        $('.teams').append(el)
+      })
+    })
+}
+
+function fetch_repos (team) {
+  $.ajax(base_url + '/teams/' + team + '/repos')
+    .done(function (data) {
+      $('.pull-requests').empty()
+
+      data.forEach(function (repo) {
+        fetch_pull_requests(repo.owner.login, repo.name)
+      })
+    })
+}
+
+function fetch_pull_requests (owner, repo) {
+  $.ajax(base_url + '/repos/' + owner + '/' + repo + '/pulls')
+    .done(function (data) {
+      data.forEach(function (pull) {
+        var el = $("<div></div>")
+        el.text(pull.title)
+        $('.pull-requests').append(el)
+      })
+    })
+}
+
+$('.orgs').on('click', function (e) {
+    var el = $(e.target)
+    var id = el.data("id")
+
+    fetch_teams(id);
+});
+
+$('.teams').on('click', function (e) {
+    var el = $(e.target)
+    var id = el.data("id")
+
+    fetch_repos(id)
+});
+
+$(function() {
+  fetch_orgs()
 });
