7a07dbfdeabe4823f945ae651dc42d10fb53fb3b
Francesco Gigli <jaramir@gmail.com>
2017-03-23 16:07:12 +0000

    update PRs

--- public/app.js
+++ public/app.js
@@ -9,68 +9,69 @@ $.ajaxSetup({
 });
 var base_url = 'https://api.github.com';
 
-function fetch_orgs () {
+
+$(function() {
   $.ajax(base_url + '/user/orgs')
     .done(function (data) {
       data.forEach(function (org) {
-        var el = $("<div></div>")
+        var el = $('<div></div>')
         el.text(org.login)
-        el.data("id", org.login)
+        el.data('id', org.login)
         $('.orgs').append(el)
       })
     })
+});
+
+$('.orgs').on('click', function (e) {
+    var el = $(e.target)
+    var id = el.data('id')
+
+    $.ajax(base_url + '/orgs/' + id + '/teams')
+      .done(function (data) {
+        $('.orgs').empty()
+
+        data.forEach(function (team) {
+          var el = $('<div></div>')
+          el.text(team.name)
+          el.data('id', team.id)
+          $('.teams').append(el)
+        })
+      })
+});
+
+$('.teams').on('click', function (e) {
+    var el = $(e.target)
+    var id = el.data('id')
+
+    $.ajax(base_url + '/teams/' + id + '/repos')
+      .done(function (data) {
+        $('.teams').empty()
+
+        setup_timer(data)
+      })
+});
+
+function setup_timer (repos) {
+  setInterval(update, 5 * 60 * 1000, repos)
+  update(repos)
 }
 
-function fetch_teams (org) {
-  $.ajax(base_url + '/orgs/' + org + '/teams')
-    .done(function (data) {
-      $('.teams').empty()
+function update (repos) {
+  console.log('updating..')
+  $('.pull-requests').empty()
 
-      data.forEach(function (team) {
-        var el = $("<div></div>")
-        el.text(team.name)
-        el.data("id", team.id)
-        $('.teams').append(el)
-      })
-    })
-}
-
-function fetch_repos (team) {
-  $.ajax(base_url + '/teams/' + team + '/repos')
-    .done(function (data) {
-      $('.pull-requests').empty()
-
-      data.forEach(function (repo) {
-        fetch_pull_requests(repo.owner.login, repo.name)
-      })
-    })
+  repos.forEach(function (repo) {
+    fetch_pull_requests(repo.owner.login, repo.name)
+  })
 }
 
 function fetch_pull_requests (owner, repo) {
   $.ajax(base_url + '/repos/' + owner + '/' + repo + '/pulls')
     .done(function (data) {
       data.forEach(function (pull) {
-        var el = $("<div></div>")
+        var el = $('<div></div>')
         el.text(pull.title)
         $('.pull-requests').append(el)
       })
     })
 }
-
-$('.orgs').on('click', function (e) {
-    var el = $(e.target)
-    var id = el.data("id")
-
-    fetch_teams(id);
-});
-
-$('.teams').on('click', function (e) {
-    var el = $(e.target)
-    var id = el.data("id")
-
-    fetch_repos(id)
-});
-
-$(function() {
-  fetch_orgs()
-});
