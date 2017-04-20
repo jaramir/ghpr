4bef2bd50b930d4decf58165bea83e3d024e5877
Francesco Gigli <jaramir@gmail.com>
2017-04-20 12:30:07 +0200

    reimplemented in react (#1)
    * showing orgs
    
    * clicking orgs
    
    * no longer ignoring yarn.lock
    
    * using react, abandon flexbox
    
    * fetching PRs on a schedule
    
    * renders pull requests
    
    * feature parity

--- src/app.js
+++ src/app.js
@@ -1,87 +1,143 @@
-var access_token = document.location.search.substr(1);
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-$.ajaxSetup({
-  accepts: {
-    'github': 'application/vnd.github.v3+json'
-  },
-  headers: {
-    'Authorization': 'token ' + access_token
-  }
-});
-var base_url = 'https://api.github.com';
-
-
-$(() => {
-  $.ajax(base_url + '/user/orgs')
-    .done(data => {
-      data.forEach(org => {
-        var el = $('<div></div>')
-        el.text(org.login)
-        el.data('id', org.login)
-        $('.orgs').append(el)
-      })
-    })
-});
-
-$('.orgs').on('click', e => {
-    var el = $(e.target)
-    var id = el.data('id')
-
-    $.ajax(base_url + '/orgs/' + id + '/teams')
-      .done(data => {
-        $('.orgs').empty()
-
-        data.forEach(team => {
-          var el = $('<div></div>')
-          el.text(team.name)
-          el.data('id', team.id)
-          $('.teams').append(el)
-        })
-      })
-});
-
-$('.teams').on('click', e => {
-    var el = $(e.target)
-    var id = el.data('id')
-
-    $.ajax(base_url + '/teams/' + id + '/repos')
-      .done(data => {
-        $('.teams').empty()
-
-        setup_timer(data)
-      })
-});
-
-function setup_timer (repos) {
-  setInterval(update, 5 * 60 * 1000, repos)
-  update(repos)
+const accessToken = document.location.search.substr(1)
+const baseUrl = 'https://api.github.com'
+const options = {
+    headers: {
+        'Authorization': 'token ' + accessToken,
+        'Accept': 'application/vnd.github.v3+json'
+    }
 }
 
-function update (repos) {
-  console.log('updating..')
-  $('.pull-requests').empty()
+const Selector = (props) => <div>
+    {props.options.map(option =>
+        <div style={{display: "inline-block"}} key={props.getId(option)}>
+            <button
+                style={{
+                    padding: ".5em",
+                    margin: ".5em",
+                    whiteSpace: "nowrap",
+                    fontSize: "large"
+                }}
+                onClick={() => props.onSelect(props.getId(option))}>
+                {props.getText(option)}
+            </button>
+        </div>
+    )}
+</div>
 
-  repos.forEach(repo => {
-    fetch_pull_requests(repo.owner.login, repo.name)
-  })
+const OrgSelector = (props) => <Selector
+    options={props.orgs}
+    getId={org => org.login}
+    getText={org => org.login}
+    onSelect={props.onOrgSelected}/>
+
+const TeamSelector = (props) => <Selector
+    options={props.teams}
+    getId={team => team.id}
+    getText={team => team.name}
+    onSelect={props.onTeamSelected}/>
+
+function flatten(arrayOfArrays) {
+    return arrayOfArrays.reduce(
+        (acc, cur) => acc.concat(cur),
+        []
+    )
 }
 
-function fetch_pull_requests (owner, repo) {
-  $.ajax(base_url + '/repos/' + owner + '/' + repo + '/pulls')
-    .done(data => {
-      data.forEach(pull => {
-        var avatar = $('<img />')
-        avatar.attr('src', pull.user.avatar_url)
+class Ghpr extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            orgs: [],
+            org: null,
+            teams: [],
+            team: null,
+            repos: [],
+            pullRequests: [],
+        }
+    }
 
-        var link = $('<a />')
-        link.attr('href', pull.html_url)
-        link.attr('target', '_blank')
-        link.text(pull.title)
+    componentWillMount() {
+        fetch(baseUrl + '/user/orgs', options)
+            .then(reponse => reponse.json()
+                .then(orgs => this.setState(Object.assign({}, this.state, {orgs}))))
+    }
 
-        var el = $('<div />')
-        el.append(avatar)
-        el.append(link)
-        $('.pull-requests').append(el)
-      })
-    })
+    onOrgSelected(login) {
+        this.setState(Object.assign({}, this.state, {org: login}))
+        fetch(baseUrl + '/orgs/' + login + '/teams', options)
+            .then(reponse => reponse.json()
+                .then(teams => this.setState(Object.assign({}, this.state, {teams}))))
+    }
+
+    onTeamSelected(id) {
+        this.setState(Object.assign({}, this.state, {team: id}))
+
+        fetch(baseUrl + '/teams/' + id + '/repos', options)
+            .then(reponse => reponse.json()
+                .then(repos => {
+                    this.setState(Object.assign({}, this.state, {repos}))
+                    this.update()
+                    this.setupTimer()
+                }))
+
+    }
+
+    setupTimer() {
+        setInterval(this.update.bind(this), 5 * (60-59) * 1000)
+    }
+
+    update() {
+        Promise.all(
+            this.state.repos.map(repo =>
+                fetch(`${baseUrl}/repos/${repo.owner.login}/${repo.name}/pulls`, options)
+                    .then(response => response.json())))
+            .then(reponses => {
+                let pullRequests = flatten(reponses)
+                this.setState(Object.assign({}, this.state, {pullRequests}))
+            })
+    }
+
+    render() {
+        return <div>
+            { this.state.org === null &&
+                <OrgSelector
+                    orgs={this.state.orgs}
+                    onOrgSelected={this.onOrgSelected.bind(this)}/>
+            }
+            { this.state.org !== null && this.state.team === null &&
+                <TeamSelector
+                    teams={this.state.teams}
+                    onTeamSelected={this.onTeamSelected.bind(this)}/>
+            }
+            { this.state.org !== null && this.state.team !== null &&
+                this.state.pullRequests.map(pr =>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        border: "1px solid lightgrey",
+                        padding: ".5em",
+                        margin: ".5em",
+                        backgroundColor: "white",
+                        borderRadius: "4px"
+                    }}>
+                        <img src={pr.user.avatar_url} style={{height: "50px", width: "50px"}}/>
+                        <a style={{
+                            textDecoration: "none",
+                            color: "#02779E",
+                            paddingLeft: ".5em"
+                        }} href={pr.html_url} target="_blank">{pr.title}</a>
+                    </div>
+                )
+            }
+        </div>
+    }
 }
+
+ReactDOM.render(
+    <Ghpr />,
+    document.getElementById('root')
+);
