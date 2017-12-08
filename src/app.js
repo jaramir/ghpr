8b5cdfefedd5f3602c04b7d7f66c8b53df874755
Francesco Gigli <jaramir@gmail.com>
2017-12-08 12:17:13 +0000

    fetch the repos at each update

--- src/app.js
+++ src/app.js
@@ -73,16 +73,9 @@ class Ghpr extends React.Component {
     }
 
     onTeamSelected(id) {
-        this.setState(Object.assign({}, this.state, {team: id}))
-
-        fetch(baseUrl + '/teams/' + id + '/repos?per_page=100', options)
-            .then(reponse => reponse.json()
-                .then(repos => {
-                    this.setState(Object.assign({}, this.state, {repos}))
-                    this.update()
-                    this.setupTimer()
-                }))
-
+        const newState = Object.assign({}, this.state, {team: id})
+        this.setState(newState, this.update)
+        this.setupTimer()
     }
 
     setupTimer() {
@@ -90,16 +83,18 @@ class Ghpr extends React.Component {
     }
 
     update() {
-        Promise.all(
-            this.state.repos
-            .filter(repo => repo.permissions.admin)
-            .map(repo =>
-                fetch(`${baseUrl}/repos/${repo.owner.login}/${repo.name}/pulls`, options)
-                    .then(response => response.json())))
-            .then(reponses => {
-                let pullRequests = flatten(reponses)
-                this.setState(Object.assign({}, this.state, {pullRequests}))
-            })
+        fetch(baseUrl + '/teams/' + this.state.team + '/repos?per_page=100', options)
+            .then(reponse => reponse.json())
+            .then(repos =>
+                Promise.all(repos
+                    .filter(repo => repo.permissions.admin)
+                    .map(repo =>
+                        fetch(`${baseUrl}/repos/${repo.owner.login}/${repo.name}/pulls`, options)
+                            .then(response => response.json())))
+                    .then(reponses => {
+                        let pullRequests = flatten(reponses)
+                        this.setState(Object.assign({}, this.state, {pullRequests}))
+                    }))
     }
 
     render() {
@@ -118,7 +113,8 @@ class Ghpr extends React.Component {
                 this.state.pullRequests.map(pr =>
                     <div style={{display: "flex", flexDirection: "row", alignItems: "center",
                         border: "1px solid lightgrey", borderRadius: "4px",
-                        margin: ".5em", backgroundColor: "white"}}>
+                        margin: ".5em", backgroundColor: "white"}}
+                        key={pr.id}>
                         <img src={pr.user.avatar_url} style={{height: "50px", width: "50px", margin: ".5em"}}/>
                         <div style={{display: "flex", flexDirection: "column"}}>
                             <a style={{textDecoration: "none", color: "#02779E"}}
