94df742c525d6d838ab2035ee8f385f7a9171483
Francesco Gigli <jaramir@gmail.com>
2020-07-31 17:58:40 +0200

    switch to fetch

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const client_secret = process.env.CLIENT_SECRET;
 const client_id = '961e6abf281bd56388ff'
-const request = require('request');
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
 
 const server = express();
 
@@ -9,22 +10,23 @@ server.use('/', express.static(__dirname + '/public'));
 
 server.use('/oauth-callback', (req, res) => {
   const code = req.query.code;
+  const params = new URLSearchParams();
+  params.append('client_id', client_id);
+  params.append('client_secret', client_secret);
+  params.append('code', code);
 
   const options = {
-    url: 'https://github.com/login/oauth/access_token',
+    method: 'POST',
+    body: params,
     headers: {
       'Accept': 'application/json'
-    },
-    form: {
-      client_id,
-      client_secret,
-      code
     }
   }
 
-  request.post(options, (err, response, body) => {
-    res.redirect('/app.html?' + JSON.parse(body).access_token)
-  })
+  fetch('https://github.com/login/oauth/access_token', options)
+    .then(data => data.json())
+    .then(json => res.redirect('/app.html?' + json.access_token)
+    )
 });
 
 server.listen(process.env.PORT);
